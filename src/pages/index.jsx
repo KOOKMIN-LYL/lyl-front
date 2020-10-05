b39ffd08124ae793d24d8559129ff2d789685511
l <jh>
2020-10-05 13:23:23 +0900

    master branch 초기화

--- src/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-export { default as Main } from 'pages/Main';
-export { default as Login } from 'pages/Login';
-export { default as Signup } from 'pages/Signup';
-export { default as Product } from 'pages/Product';
-export { default as Cart } from 'pages/Cart';
-export { default as Order } from 'pages/Order';
